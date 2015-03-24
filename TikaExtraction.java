611497249310d45dccb3e06e5837ab8e3a0c0aaa
bond99 <ankit.yadav.iiith@gmail.com>
2015-03-25 03:53:44 +0530

    Modified Tika Extraction

--- TikaExtraction.java
+++ TikaExtraction.java
@@ -1,21 +1,60 @@
-import java.io.File;
-import java.io.IOException;
-
-import org.apache.tika.Tika;
-import org.apache.tika.exception.TikaException;
-import org.xml.sax.SAXException;
-
-import com.mashape.unirest.http.exceptions.UnirestException;
-
-public class TikaExtraction {
-	public static void main(final String[] args) throws IOException,
-			SAXException, TikaException, UnirestException {
-
-		File file = new File(
-				"C:/Users/SAMEER/Desktop/Game-Skill-Improvements-using-Raspberry-PI.docx");
-		Tika tika = new Tika();
-		String filecontent = tika.parseToString(file);
-		System.out.println("Extracted Content: " + filecontent);
-	}
-
+import java.io.ByteArrayOutputStream;  
+import java.io.File;  
+import java.io.InputStream;  
+import java.io.OutputStream;  
+import java.io.OutputStreamWriter;  
+import java.net.URL;  
+   
+import org.apache.tika.detect.DefaultDetector;  
+import org.apache.tika.detect.Detector;  
+import org.apache.tika.io.TikaInputStream;  
+import org.apache.tika.metadata.Metadata;  
+import org.apache.tika.parser.AutoDetectParser;  
+import org.apache.tika.parser.ParseContext;  
+import org.apache.tika.parser.Parser;  
+import org.apache.tika.sax.BodyContentHandler;  
+   
+import org.xml.sax.ContentHandler;  
+   
+class TextExtractor {  
+  private OutputStream outputstream;  
+  private ParseContext context;  
+  private Detector detector;  
+  private Parser parser;  
+  private Metadata metadata;  
+  private String extractedText;  
+   
+  public TextExtractor() {  
+    context = new ParseContext();  
+    detector = new DefaultDetector();  
+    parser = new AutoDetectParser(detector);  
+    context.set(Parser.class, parser);  
+    outputstream = new ByteArrayOutputStream();  
+    metadata = new Metadata();  
+  }  
+   
+  public void process(String filename) throws Exception {  
+    URL url;  
+    File file = new File(filename);  
+    if (file.isFile()) {  
+      url = file.toURI().toURL();  
+    } else {  
+      url = new URL(filename);  
+    }  
+    InputStream input = TikaInputStream.get(url, metadata);  
+    ContentHandler handler = new BodyContentHandler(outputstream);  
+    parser.parse(input, handler, metadata, context);  
+    input.close();  
+  }  
+   
+      
+  public static void main(String args[]) throws Exception {  
+    if (args.length == 1) {  
+      TextExtractor textExtractor = new TextExtractor();
+      System.getProperties().put("http.proxyHost", "proxy.iiit.ac.in");
+	  System.getProperties().put("http.proxyPort", "8080");  
+      textExtractor.process(args[0]);  
+      textExtractor.getString();  
+    }   
+  }  
 }
